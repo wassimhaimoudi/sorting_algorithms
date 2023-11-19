55f76481c1b7d4427b0e0a70fd70654963bb6544
wassimhaimoudi <wassimhaimoudi1@gmail.com>
2023-11-19 14:23:16 -0800

    Adding Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,27 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, flag;
 
-	i = 0;
-	while (i < size)
+	if (array)
 	{
-		flag = 0;
-		for (j = 0; j <= size - 2; j++)
+		i = 0;
+		while (i < size)
 		{
-			if (array[j] > array[j + 1])
+			flag = 0;
+			for (j = 0; j <= size - 2; j++)
 			{
-				swap(array, (int)j, (int)(j + 1));
-				print_array(array, size);
-				flag = 1;
+				if (array[j] > array[j + 1])
+				{
+					swap(array, (int)j, (int)(j + 1));
+					print_array(array, size);
+					flag = 1;
+				}
 			}
+			if (flag == 0)
+			{
+				break;
+			}
+			i++;
 		}
-		if (flag == 0)
-		{
-			break;
-		}
-		i++;
 	}
 }
 
